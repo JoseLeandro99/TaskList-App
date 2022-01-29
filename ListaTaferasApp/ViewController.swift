ca81ce6af553a987e33334ed0c201f2373dcc251
Leandro BS <joleandrobruno@gmail.com>
2022-01-28 16:16:15 -0800

    Initial Commit

---
+++ ListaTaferasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListaTaferasApp
+//
+//  Created by Dev on 28/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
